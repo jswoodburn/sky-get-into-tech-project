f8bba3d4a18bee3f661bd858ac3d4f6fe97b39d6
Jessie Auguste <jessieauguste@gmail.com>
2021-04-15 09:21:20 +0100

    adding more python script for user creation and log in

--- application/usercreation/login.py
+++ application/usercreation/login.py
@@ -1,3 +1,32 @@
+from flask import Flask, session, redirect, url_for, request
+from markupsafe import escape
+app = Flask(__name__)
+app.secret_key = Flask.secret_key
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
 {% extends 'base.html' %}
 
 {% block header %}
@@ -13,3 +42,6 @@
     <input type="submit" value="Log In">
   </form>
 {% endblock %}
+
+login_manager = LoginManager()
+login_manager.init_app(app)
