838cfba511d6d732c02804a6d41d95eda28ac91e
Jessie Auguste <jessieauguste@gmail.com>
2021-04-19 18:12:12 +0100

    add changes

--- application/__init__.py
+++ application/__init__.py
@@ -5,9 +5,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:password@localhost/digital_health"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:password@localhost/digital_health"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://127.0.0.1:3306/?user=root"
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
+
 
 # Linking app to the persistence layer
 db = SQLAlchemy(app)
--- application/create.py
+++ application/create.py
@@ -9,6 +9,7 @@ db.create_all()
 # input sample data here
 test_user_1 = User(email='jackie.woodburn@gmail.com', password='password1', phone_number='07512656497',
                    first_name='Jackie', last_name='Woodburn')
+test_journal = Journal(journal_id=234324)
 
 
 # add and commit sample data to db here
