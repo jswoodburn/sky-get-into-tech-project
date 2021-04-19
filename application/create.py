11969cd0f9ff44bcf9e6ef1a0250a3738c15e63e
Jessie Auguste <jessieauguste@gmail.com>
2021-04-19 21:10:27 +0100

    can create tables using SQLAlchemy

--- application/create.py
+++ application/create.py
@@ -1,13 +1,12 @@
 from application import db
-from models import Journal, User
+from models import User
 # EVERYONE RUN THIS TO GET DBS SYNCED UP
 db.drop_all()
 db.create_all()
 
 # input sample data here
-test_user_1 = User(email='jackie.woodburn@gmail.com', password='password1', phone_number='07512656497',
+test_user_1 = User(email='jackie.woodburn@gmail.com',
                    first_name='Jackie', last_name='Woodburn')
-test_journal = Journal(journal_id=234324)
 
 
 # add and commit sample data to db here
