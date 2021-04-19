729e470c7e3aa9e1b51f72aa39f0389d28225979
Jessie Auguste <jessieauguste@gmail.com>
2021-04-19 08:13:23 +0100

    google login development

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
 from application import app
-from flask import Flask
+import json
+import os
+# Third-party libraries
+from flask import Flask, redirect, request, url_for
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+#Google Config
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
 app.config['SECRET_KEY'] = 'skygit'
+
 if __name__ == "__main__":
     app.run(debug=True)
