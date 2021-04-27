0fc0aab96674e5b455afb9b8eb677a986e7c4d29
Suh <hasuh@hotmail.co.uk>
2021-04-27 09:26:24 +0100

    comments on Layout2. try to get sqlalchemy working

--- application/__init__.py
+++ application/__init__.py
@@ -10,3 +10,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:password@localhost
 #Linking app to the persistance layer
 db = SQLAlchemy(app)
 
+# Python standard libraries
+import json
+import os
+
+
+# Third party libraries
+from flask import Flask, redirect, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+username = 'newuser'
+password = 'password'
+userpass = 'mysql+pymysql://' + username + ':' + password + '@'
+server = '127.0.0.1'
+dbname = '/db'
+
+# put them all together as a string that shows SQLAlchemy where the database is
+app.config['SQLALCHEMY_DATABASE_URI'] = userpass + server + dbname
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://newuser:password@localhost/db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
