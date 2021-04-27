57ad085ad5f788cd628e4989c5f6651f5c991b77
Suh <hasuh@hotmail.co.uk>
2021-04-27 15:54:17 +0100

    add images

--- application/__init__.py
+++ application/__init__.py
@@ -5,26 +5,45 @@ app = Flask(__name__)
 
 from application import routes
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:password@localhost/flask_test"
-
-#Linking app to the persistance layer
-db = SQLAlchemy(app)
-
-# Python standard libraries
-import json
-import os
-
-
-# Third party libraries
-from flask import Flask, redirect, request, url_for
-from flask_sqlalchemy import SQLAlchemy
-username = 'newuser'
-password = 'password'
-userpass = 'mysql+pymysql://' + username + ':' + password + '@'
-server = '127.0.0.1'
-dbname = '/db'
-
-# put them all together as a string that shows SQLAlchemy where the database is
-app.config['SQLALCHEMY_DATABASE_URI'] = userpass + server + dbname
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://newuser:password@localhost/db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# this is for the hidden tag in html forms
+app.config['SECRET_KEY'] = 'SECRET_KEY'
+
+# link app to the persistence layer
+db = SQLAlchemy(app)
+
+
+
+# our current code
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+#
+# app = Flask(__name__)
+#
+# from application import routes
+#
+# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:password@localhost/flask_test"
+#
+# #Linking app to the persistance layer
+# db = SQLAlchemy(app)
+#
+# # Python standard libraries
+# import json
+# import os
+#
+#
+# # Third party libraries
+# from flask import Flask, redirect, request, url_for
+# from flask_sqlalchemy import SQLAlchemy
+# username = 'newuser'
+# password = 'password'
+# userpass = 'mysql+pymysql://' + username + ':' + password + '@'
+# server = '127.0.0.1'
+# dbname = '/db'
+#
+# # put them all together as a string that shows SQLAlchemy where the database is
+# app.config['SQLALCHEMY_DATABASE_URI'] = userpass + server + dbname
+# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://newuser:password@localhost/db"
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
--- application/create.py
+++ application/create.py
@@ -14,9 +14,9 @@ test_user_1 = User(email='jackie.woodburn@gmail.com',
 # add and commit sample data to db here
 db.session.add(test_user_1)
 db.session.commit()
-[19:18]
-email=akdhsfkashdf number= ajsdflkajsfd
-[19:22]
+# [19:18]
+# email=akdhsfkashdf number= ajsdflkajsfd
+# [19:22]
 # instantiate Flask object (app)
 app = Flask(name)
 app.secret_key = 'skyGIT'
