01e2924ad488fd1e9a92cb72e8d573be742e3474
Jessie Auguste <jessieauguste@gmail.com>
2021-05-01 14:50:57 +0100

    added journal themes

--- application/create.py
+++ application/create.py
@@ -25,10 +25,19 @@ test_journal_2 = Journal(date=datetime.now().date(), time=datetime.now().time(),
                                                                                                     "register as "
                                                                                                     "deleted",
                          title="Test Entry 2 - deleted", deleted=True)
-# themes = JournalTheme(theme=['Reflection', 'Gratitude', 'Awareness of surroundings'])
+themes = [
+    JournalTheme(theme='What things make me feel alive and fulfilled?'),
+    JournalTheme(theme='What things in my life can I be grateful for today?'),
+    JournalTheme(theme='Inner Strength'),
+    JournalTheme(theme='Self Care'),
+    JournalTheme(theme='Checking in with yourself'),
+    JournalTheme(theme='What might challenge me today?'),
+    JournalTheme(theme='What are my most important values?'),
+    JournalTheme(theme='How am I impacting other people around me daily?'),
+    JournalTheme(theme='Connection')]
 
 # add and commit sample data to db here
-# db.session.add(themes)
+db.session.add(themes)
 db.session.add(test_journal_1)
 db.session.add(test_journal_2)
 db.session.commit()
